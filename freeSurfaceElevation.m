34c9830b161fc47c7f3be00eb3705176b67e2cb3
mcpaprota <105712326+mcpaprota@users.noreply.github.com>
2023-02-14 13:56:07 +0100

    Update freeSurfaceElevation.m

--- freeSurfaceElevation.m
+++ freeSurfaceElevation.m
@@ -10,7 +10,7 @@ function eta = freeSurfaceElevation(eta_hat,kappa,x)
 % eta - free surface elevation (m)
 %
 % Author: Maciej Paprota
-% Reference: M. Paprota. 2022. A twin wavemaker model for liquid sloshing in a rectangular tank.
+% Reference: M. Paprota. 2023. A twin wavemaker model for liquid sloshing in a rectangular tank. Ocean Engineering, 272, 113919
 
-eta = eta_hat*cos(kappa'*x); % eq. (2.2)
+eta = eta_hat*cos(kappa'*x); % eq. (2)
 end
