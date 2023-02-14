cff4d01e46d7c770fb9ce5f531d9554c72cea902
mcpaprota <105712326+mcpaprota@users.noreply.github.com>
2023-02-14 14:02:15 +0100

    Update velocityComponents.m

--- velocityComponents.m
+++ velocityComponents.m
@@ -14,15 +14,15 @@ function [u, w, X, Z] = ...
 % u, w - horizontal and vertical velocity components (m/s)
 %
 % Author: Maciej Paprota
-% Reference: M. Paprota. 2022. A twin wavemaker model for liquid sloshing in a rectangular tank.
+% Reference: M. Paprota. 2023. A twin wavemaker model for liquid sloshing in a rectangular tank. Ocean Engineering, 272, 113919
 %
 % Initialization
 [X,Z,Kappa] = meshgrid(x,z,kappa);
 [~,~,Phi_hat] = meshgrid(x,z,phi_hat);
-u = -notTwin*xi*X(:,:,1)/l+xi-... % conditionally resolved eqs. (2.20) and (2.35)
+u = -notTwin*xi*X(:,:,1)/l+xi-... % conditionally resolved eqs. (20) and (35)
     sum(Phi_hat.*Kappa.*cosh(Kappa.*(Z+d))./...
     cosh(Kappa*d).*sin(Kappa.*X),3);
-w = notTwin*xi*(Z(:,:,1)+d)/l+sum(Phi_hat.*Kappa.*sinh(Kappa.*(Z+d))./... % conditionally resolved eqs. (2.21) and (2.36)
+w = notTwin*xi*(Z(:,:,1)+d)/l+sum(Phi_hat.*Kappa.*sinh(Kappa.*(Z+d))./... % conditionally resolved eqs. (21) and (36)
     cosh(Kappa*d).*cos(Kappa.*X),3);
 X = X(:,:,1); Z = Z(:,:,1);
 end
